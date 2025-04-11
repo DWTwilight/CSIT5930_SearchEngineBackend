b7223754d69761e97520bedaaa800831b1b4123e
Tianci Zhao <446416074@qq.com>
2025-04-11 16:28:34 +0800

    initial commit

---
+++ src/test/java/com/hkust/csit5930/searchengine/SearchEngineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkust.csit5930.searchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
